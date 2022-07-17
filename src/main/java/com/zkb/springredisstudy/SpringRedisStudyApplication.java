10f88460d5ea2b9cd2202037801e4b7636a8b309
zhengkangbo <zkb2668977574@163.com>
2022-07-17 23:59:09 +0800

    测试提交

---
+++ src/main/java/com/zkb/springredisstudy/SpringRedisStudyApplication.java
@@ -0,0 +1,13 @@
+package com.zkb.springredisstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zkb/springredisstudy/SpringRedisStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zkb.springredisstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
