3aece555623b92abe7e97cb1622e991e2f176f15
zhengkangbo <zhengkangbo@bianfeng.com>
2022-10-14 16:43:25 +0800

    解决依赖问题，自定义负载均衡规则

--- order/src/main/java/com/stu/order/rule/MyRule.java
+++ order/src/main/java/com/stu/order/rule/MyRule.java
@@ -3,18 +3,18 @@ package com.stu.order.rule;
 import com.netflix.client.config.IClientConfig;
 import com.netflix.loadbalancer.AbstractLoadBalancerRule;
 import com.netflix.loadbalancer.Server;
+import org.apache.commons.lang.math.RandomUtils;
 
 import java.util.List;
 
 public class MyRule extends AbstractLoadBalancerRule {
 
 
-
     @Override
     public Server choose(Object key) {
         List<Server> allServers = getLoadBalancer().getAllServers();
-        System.out.println(allServers.toArray());
-        return allServers.get(0);
+        int i = RandomUtils.nextInt(allServers.size());
+        return allServers.get(i);
     }
 
     @Override
